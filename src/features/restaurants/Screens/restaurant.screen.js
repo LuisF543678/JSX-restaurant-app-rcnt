79a22b035c48c51f2ddcb48b1c2582ab9433e281
Luisf543678 <luisf543678@gmail.com>
2021-09-27 11:53:58 -0500

    version new

--- src/features/restaurants/Screens/restaurant.screen.js
+++ src/features/restaurants/Screens/restaurant.screen.js
@@ -1,45 +1,43 @@
 import React from "react";
 import {
-    SafeAreaView,
-    View,
-    Text,
-    StyleSheet,
-    Platform,
-    StatusBar
-} from "react-native"
-import { Searchbar } from 'react-native-paper';
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  StatusBar,
+} from "react-native";
+import { Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.components";
 import styled from "styled-components/native";
-
 
 //console.log(Platform);
 //const isAndroid = Platform.OS === "android";
 //console.log(Platform.OS === "android");
 console.log(StatusBar.currentHeight);
 const SafeArea = styled(SafeAreaView)`
-    flex: 1;
-    ${StatusBar.currentHeight} && 'margin-top: ${StatusBar.currentHeight}px';
+  flex: 1;
+  ${StatusBar.currentHeight} && 'margin-top: ${StatusBar.currentHeight}px';
 `;
 
 const SearchContainer = styled(View)`
-    padding: 16px;
+  padding: 16px;
 `;
 
 const RestaurantListContainer = styled(View)`
-    flex: 1;
-    padding: 16px;
+  flex: 1;
+  padding: 16px;
 `;
 
 export const RestaurantScreen = () => {
-    return (
-        <SafeArea >
-            <SearchContainer>
-                <Searchbar />
-            </SearchContainer>
-            <RestaurantListContainer>
-                <RestaurantInfoCard />
-            </RestaurantListContainer>
-        </SafeArea>
-    );
+  return (
+    <SafeArea>
+      <SearchContainer>
+        <Searchbar />
+      </SearchContainer>
+      <RestaurantListContainer>
+        <RestaurantInfoCard />
+      </RestaurantListContainer>
+    </SafeArea>
+  );
 };
-
--- src/features/restaurants/components/restaurant-info-card.components.js
+++ src/features/restaurants/components/restaurant-info-card.components.js
@@ -19,13 +19,13 @@ const RestaurantCardCover = styled(Card.Cover)`
  * font-family: ${(props) => props.theme.fontSizes.caption};
  */
 const Title = styled.Text`
-font-size:16px;
+  font-size: 16px;
   color: ${(props) => props.theme.colors.text.primary};
 `;
 
 /**
- *  @method match with const Address
- *  non-functional code
+ * @method match with const Address
+ * non-functional code
  * font-family: ${(props) => props.theme.fonts.heading},
  * font-family: ${(props) => props.theme.fontSizes.caption};
  */
@@ -45,16 +45,16 @@ const Star = styled(Card.Cover)`
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
-    name = "Some nice restaurant",
+    name = "Susi nice restaurant",
     icon,
     photos = [
       "https://cdn.pixabay.com/photo/2016/11/18/14/05/brick-wall-1834784_960_720.jpg",
     ],
     address = "Some random street",
     isOpenNow = true,
-    rating = 3.5,
+    rating = 4,
     isClosedTemporaly,
-    stars = ["https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Star_icon_stylized.svg/1077px-Star_icon_stylized.svg.png"],
+    stars = require("../../../../assets/star.ico"),
   } = restaurant;
 
   /**
@@ -68,9 +68,10 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
       <Info>
         <Title>{name}</Title>
         <div>
-          {ratingArray.map(() => (
-            <Star source={{ uri: stars[0] }} />
+          {ratingArray.map((str) => (
+            <Star source={{ uri: stars }} />
           ))}
+          <Title>{rating}</Title>
         </div>
         <Address>{address}</Address>
       </Info>
